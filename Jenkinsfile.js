e27b9de9c880bac99a1efa5bd3b751d9301ea4de
draja27 <145457249+draja27@users.noreply.github.com>
2023-09-26 23:55:38 +0530

    Update Jenkinsfile.js

--- Jenkinsfile.js
+++ Jenkinsfile.js
@@ -4,12 +4,28 @@ pipeline {
     stages {
         stage('Checkout') {
             steps {
-                // Checkout code from Git
-                git credentialsId: 'your-git-credentials', url: 'https://github.com/draja27/deepak.git'
+                // Checkout the code from the Git repository
+                script {
+                    def scmVars = 'https://github.com/draja27/deepak.git'
+                    // Optionally, you can print out some Git information
+                    echo "Checked out Git branch: ${scmVars.GIT_BRANCH}"
+                    echo "Git commit: ${scmVars.GIT_COMMIT}"
+                }
             }
         }
 
-        
+        stage('Build') {
+            steps {
+                // Replace this with your build steps (e.g., compiling, packaging)
+                sh 'echo "Building the project"'
+            }
+        }
+
+        stage('Test') {
+            steps {
+                // Replace this with your testing commands
+                sh 'echo "Running tests"'
+            }
         }
     }
-
+}
